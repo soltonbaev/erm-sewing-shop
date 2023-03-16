045a712b61be83f4e05386cc5308caec821310b9
soltonbaev <isoltonbaev@gmail.com>
2023-03-16 15:08:08 +0600

    setup a barebone react projec with firebase account initialization

---
+++ src/helpers/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: 'AIzaSyAOSG5knW73dt05AvsRjGq_BI3iSuX0c08',
+   authDomain: 'erm-sewing-shop.firebaseapp.com',
+   projectId: 'erm-sewing-shop',
+   storageBucket: 'erm-sewing-shop.appspot.com',
+   messagingSenderId: '642838847032',
+   appId: '1:642838847032:web:f1bf0d89bd0f429c1dd0cd',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
